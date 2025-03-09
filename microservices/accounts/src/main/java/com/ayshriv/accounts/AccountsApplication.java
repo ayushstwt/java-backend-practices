d0b994126ea1e0625493d0e1adc827cda4006c03
ayushstwt <ayusrivastava2k3@gmail.com>
2025-03-10 00:44:10 +0530

    "documentation is completed."

--- microservices/accounts/src/main/java/com/ayshriv/accounts/AccountsApplication.java
+++ microservices/accounts/src/main/java/com/ayshriv/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.ayshriv.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "Ayshriv Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Ayush Shrivastava",
+                        email = "ayshriv2k3@gmail.com",
+                        url = "https://www.ayshriv.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.ayshriv.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description =  "Ayshriv Accounts microservice REST API Documentation",
+                url = "https://www.ayshriv.com/swagger-ui.html"
+        )
+)
 public class   AccountsApplication {
 
     public static void main(String[] args) {
--- microservices/accounts/src/main/java/com/ayshriv/accounts/dto/CustomerDto.java
+++ microservices/accounts/src/main/java/com/ayshriv/accounts/dto/CustomerDto.java
@@ -1,5 +1,6 @@
 package com.ayshriv.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -7,19 +8,34 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
 public class CustomerDto {
 
-    @NotEmpty(message = "Name should not be empty")
-    @Size(min = 2,max = 30, message = "Name should have at least 2 characters")
+    @Schema(
+            description = "Name of the customer", example = "Ayush shrivastava"
+    )
+    @NotEmpty(message = "Name can not be a null or empty")
+    @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
     private String name;
 
-    @NotEmpty(message = "Email should not be empty")
-    @Email(message = "Invalid email format")
+    @Schema(
+            description = "Email address of the customer", example = "ayshriv.com"
+    )
+    @NotEmpty(message = "Email address can not be a null or empty")
+    @Email(message = "Email address should be a valid value")
     private String email;
 
-    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should be 10 digits")
+    @Schema(
+            description = "Mobile Number of the customer", example = "9345432123"
+    )
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNumber;
 
-
+    @Schema(
+            description = "Account details of the Customer"
+    )
     private AccountsDto accountsDto;
 }
